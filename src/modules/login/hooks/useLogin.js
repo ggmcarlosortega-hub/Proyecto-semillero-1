94a2408def0f2e3d0d77729d5b8a431fb71e2e6d
jmiguelmillan01 <jmiguel.millan01@gmail.com>
2026-06-22 23:13:16 -0500

    feat: avances en el módulo de login

--- src/modules/login/hooks/useLogin.js
+++ src/modules/login/hooks/useLogin.js
@@ -1,31 +1,29 @@
 "use client"
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+
 export function useLogin() {
     const { register, handleSubmit } = useForm();
     const router = useRouter();
-    const [inicio, setIncio] = useState(true);
 
     const onSubmit = (data) => {
         console.log("Datos capturados:", data);
-
-        router.push("/");
+        console.log(data.correo);
+        localStorage.setItem("email",data.correo);
+        localStorage.setItem("contra",data.contrasena);
+        sessionStorage.setItem("email",data.correo);
+        sessionStorage.setItem("contra",data.contrasena);
+        cookieStore.set()
+        // router.push("/");
     };
 
     const cambiarPag = () => {
-        if (inicio) {
-            console.log("Acceso valido")
             router.push("/registro");
-        } else {
-            console.log("Acceso denegado")
-        }
     };
 
     return {
         register,
         handleSubmit: handleSubmit(onSubmit),
-        cambiarPag,
-        setIncio
+        cambiarPag
     };
 }
