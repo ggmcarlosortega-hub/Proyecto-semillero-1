8537dcbaf6acce958cc658957ca1b4e19e63d368
jmiguelmillan01 <jmiguel.millan01@gmail.com>
2026-06-25 22:20:37 -0500

    implementar lógica de login y registro con almacenamiento local

--- src/modules/Registro/Hooks/useRegistro.js
+++ src/modules/Registro/Hooks/useRegistro.js
@@ -1,13 +1,14 @@
 "use client"
 import { useForm } from "react-hook-form"
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 export function useRegistro() {
     const {
         register,
         handleSubmit,
     } = useForm();
+
+    const router = useRouter();
 
     const onSubmit = (data) => {
         const lista = {
@@ -16,17 +17,40 @@ export function useRegistro() {
             password: data.passwordRequerido
         };
 
-        localStorage.setItem("user3", JSON.stringify(lista));
+        const usuarios = []
+
+        //agrege a juan
+        //agrege a carlos y elimine a juan
+        //agrege a miguel y elimine a carlos
+        usuarios.push(lista)
+
+        if (localStorage.getItem("user") === null){
+            //se agrego juan
+            localStorage.setItem("user", JSON.stringify(usuarios));
+        }else{
+            //obtenemos a juan
+            //obtenemos a carlos y juan
+            const auxiliar = JSON.parse(localStorage.getItem("user")) 
+            console.log("aux",auxiliar)
+            //carlos y juan
+            //miguel carlos y juan
+            usuarios.push(auxiliar)
+            const repararjson = usuarios.flat(Infinity)
+            //carlos y juan
+            localStorage.setItem("user", JSON.stringify(repararjson));
+            
+
+            //hay que arreglar la aninados del json
+        }
+
+        console.log("usuarios",usuarios)
+
         router.push("/login");
         console.log("Lista ", lista)
     }
 
-    const router = useRouter();
+    
 
-    const cambiarPag = () => {
-            console.log("Acceso valido")
-            router.push("/login");
-    };
     return {
         register,
         handleSubmit,
